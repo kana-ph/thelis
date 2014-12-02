b2289af850b9af23506b9cbeec8fe1fe4ed694e3
Mark Lynoel Alvarez <marklynoel.alvarez@synacy.com>
2014-12-03 00:12:23 +0800

    Add tests to domain change

--- test/unit/ph/edu/pup/ascii/thelis/ThesisSpec.groovy
+++ test/unit/ph/edu/pup/ascii/thelis/ThesisSpec.groovy
@@ -3,6 +3,7 @@ package ph.edu.pup.ascii.thelis
 import grails.buildtestdata.mixin.Build
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -19,7 +20,7 @@ class ThesisSpec extends Specification {
 
     void "save should save a thesis with at least title, 1 author, course, and publish date"() {
         given:
-            def author = Author.build()
+            def author = Author.build(name: 'lalat')
 
             def thesis = new Thesis(title: 'very title', authors: [author] as Set, course: 'BSCS', publishDate: 'Dec 2014')
 
@@ -37,5 +38,24 @@ class ThesisSpec extends Specification {
 
         then:
             '' == thesis.thesisAbstract
+    }
+
+    @Unroll
+    void "save should not save if there is no #field"() {
+        given:
+            def thesis = new Thesis(title: title, author: author, course: course, publishDate: date)
+
+        when:
+            thesis.save()
+
+        then:
+            null == Thesis.get(thesis.id)
+
+        where:
+            field        || title  | author                      | course | date
+            'title'      || null   | [Author.build(name: 'kana')]| 'BSCS' | 'Dec 2014'
+            'author'     || 'meow' | null                        | 'BSIT' | 'Jan 2015'
+            'course'     || 'wubb' | [Author.build(name: 'kana')]|  null  | 'Feb 2014'
+            'publishDate'|| 'deyt' | [Author.build(name: 'kana')]| 'BSCOE'| null
     }
 }
